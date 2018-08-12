f0bd7aef03cadac90622d39eb5935ef133621dd0
Ruslan A <ruslan.absalyamov@gmail.com>
2018-08-12 22:22:53 +0300

    [jcross] Find and fill blank blocks

--- src/main/java/io/arusland/contest/spoj/JCROSS.java
+++ src/main/java/io/arusland/contest/spoj/JCROSS.java
@@ -2,6 +2,7 @@ package io.arusland.contest.spoj;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * http://www.spoj.com/problems/JCROSS
@@ -69,6 +70,12 @@ public class JCROSS {
             printMatrix(rows, colsCount);
 
             calcBlockCertainEdges(cols, rows);
+            printMatrix(rows, colsCount);
+
+            fillBlankCells(rows, cols);
+            printMatrix(rows, colsCount);
+
+            fillBlankCells(cols, rows);
             printMatrix(rows, colsCount);
         }
 
@@ -165,6 +172,124 @@ public class JCROSS {
                 line.maxEdge = emptySize > minEmptyBlock ? emptySize - minEmptyBlock : 0;
             }
         }
+
+        /**
+         * Finds and fills blank blocks fully or partly.
+         */
+        private static void fillBlankCells(BlocksLine[] lines, BlocksLine[] another) {
+            for (int i = 0; i < lines.length; i++) {
+                BlocksLine line = lines[i];
+
+                if (!line.isDone) {
+                    Block[] blocks = line.blocks;
+                    // current prev blank start index
+                    int blankLeft = -1;
+                    boolean removeLeftEdgeBlock = false;
+                    boolean removeRightEdgeBlock = false;
+                    int finishedSolidBlocks = 0;
+
+                    makeBlocksFromSingles(line);
+
+                    for (int j = 0; j < blocks.length; j++) {
+                        Block block = blocks[j];
+
+                        if (block.type == BLOCK_SOLID) {
+                            if (block.isDone()) {
+                                finishedSolidBlocks++;
+
+                                if (block.start > 0) {
+                                    if (blankLeft < 0) {
+                                        // prev solid block is not done yet
+                                        // but we can assign last cell of prev blank block
+                                        // and if it first block it must start from 0
+                                        blankLeft = j == 1 ? 0 : block.start - 1;
+                                    }
+
+                                    blocks[j - 1].fill(blankLeft, block.start - 1);
+                                    fillSingleBlocks(another, blankLeft, block.start - 1, i, BLOCK_BLANK);
+                                } else {
+                                    // remove left edge blank block if exists
+                                    removeLeftEdgeBlock = j > 0;
+                                }
+
+                                blankLeft = block.end + 1;
+                            } else {
+                                // we can't assign blank left start index
+                                // so reset it
+                                blankLeft = -1;
+                            }
+                        } else if (block.type == BLOCK_BLANK && (j + 1) == blocks.length) {
+                            // if it last blank block
+                            Block prevBlock = blocks[j - 1];
+
+                            if (prevBlock.isDone()) {
+                                if ((prevBlock.end + 1) == another.length) {
+                                    // prev solid block is real last block of the line
+                                    removeRightEdgeBlock = true;
+                                } else {
+                                    block.fill(prevBlock.end + 1, another.length - 1);
+                                    fillSingleBlocks(another, prevBlock.end + 1, another.length - 1,
+                                            i, BLOCK_BLANK);
+                                }
+                            }
+                        }
+                    }
+
+                    if (removeLeftEdgeBlock || removeRightEdgeBlock) {
+                        // remove edge blank blocks if need
+                        int startIndex = removeLeftEdgeBlock ? 1 : 0;
+                        int endIndex = removeRightEdgeBlock ? line.blocks.length - 1 : line.blocks.length;
+
+                        line.blocks = Arrays.copyOfRange(line.blocks, startIndex, endIndex);
+                    }
+
+                    if (finishedSolidBlocks == line.solidCount) {
+                        // all solid blocks fixed on their places
+                        line.isDone = true;
+                    }
+                }
+            }
+        }
+
+        /**
+         * Transform single blocks into solid blocks.
+         */
+        private static void makeBlocksFromSingles(BlocksLine line) {
+            // TODO: support solidCount > 1
+            Integer solidBlock = Integer.valueOf(BLOCK_SOLID);
+
+            if (line.singles.values().contains(solidBlock) && line.solidCount == 1) {
+                Block[] blocks = line.blocks;
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
+                Map<Integer, Integer> blankSingles = new HashMap<>();
+
+                for (int index : line.singles.keySet()) {
+                    int type = line.singles.get(index);
+
+                    if (type == BLOCK_SOLID) {
+                        min = Math.min(min, index);
+                        max = Math.max(max, index);
+                    } else {
+                        blankSingles.put(index, type);
+                    }
+                }
+
+                if (min <= max) {
+                    for (int i = 0; i < blocks.length; i++) {
+                        if (blocks[i].type == BLOCK_SOLID) {
+                            blocks[i].fill(min, max);
+                        }
+                    }
+
+                    line.singles.clear();
+
+                    if (!blankSingles.isEmpty()) {
+                        blankSingles.keySet().forEach(p -> line.singles.put(p, blankSingles.get(p)));
+                    }
+                }
+            }
+        }
     }
 
     private static BlocksLine[] readBlocks(int count, BufferedReader br) throws IOException {
@@ -193,7 +318,7 @@ public class JCROSS {
     }
 
     private static void printMatrix(BlocksLine[] lines, int lineSize) {
-        System.out.print(" ");
+        System.out.print("  ");
         for (int i = 0; i < lineSize; i++) {
             System.out.print((i + 1) % 10);
         }
@@ -203,6 +328,7 @@ public class JCROSS {
         for (int i = 0; i < lines.length; i++) {
             BlocksLine line = lines[i];
             System.out.print((i + 1) % 10);
+            System.out.print('|');
 
             for (int j = 0; j < lineSize; j++) {
                 Block block = line.findBlock(j);
@@ -221,7 +347,10 @@ public class JCROSS {
             }
             System.out.print(line.isDone ? '!' : '|');
 
-            System.out.println();
+            System.out.println(Arrays.stream(line.blocks)
+                    .filter(p -> p.type == BLOCK_SOLID)
+                    .map(p -> Integer.toString(p.size))
+                    .collect(Collectors.joining(",")));
         }
 
         System.out.println();
@@ -243,9 +372,9 @@ public class JCROSS {
     private static Character getBlockCharSingle(int type) {
         switch (type) {
             case BLOCK_BLANK:
-                return ':';
+                return '.';
             case BLOCK_SOLID:
-                return '@';
+                return '#';
             case BLOCK_FREE:
                 return '%';
             default:
@@ -289,6 +418,11 @@ public class JCROSS {
         public void fill(int start, int end) {
             this.start = start;
             this.end = end;
+
+            if (BLOCK_BLANK == type) {
+                // TODO: maybe it is not need?
+                this.size = this.end - this.start + 1;
+            }
         }
 
         @Override
@@ -299,6 +433,14 @@ public class JCROSS {
                     ", end=" + end +
                     ", size=" + size +
                     '}';
+        }
+
+        // block found its place
+        public boolean isDone() {
+            assert type == BLOCK_SOLID;
+
+            // no need to write "&& end >= 0"
+            return start >= 0 && (end - start + 1) == size;
         }
     }
 
